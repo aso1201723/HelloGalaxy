e21ad4455168ff34fb4e3c1d9905a7718dfc305f
aso1201723 <1201723@st.asojuku.ac.jp>
2014-06-18 09:54:52 +0900

    コミットし直し

---
+++ src/com/example/hellogalaxy/MsgActivity.java
@@ -0,0 +1,27 @@
+package com.example.hellogalaxy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MsgActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.msg_activity);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onPause();
+	}
+
+}
